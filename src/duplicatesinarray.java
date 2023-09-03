7d425a73e209f070281dfd06c2410bf57efcfe99
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-03 21:13:14 +0530

    leetcode 448 duplicate in array

---
+++ src/duplicatesinarray.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+    class duplicatesinarray
+    {
+        public List<Integer> findDuplicates(int[] arr)
+        {
+            List<Integer> duplicates = new ArrayList<>();
+            int i = 0;
+            while (i < arr.length)
+            {
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct])
+                {
+                    swap(arr, i, correct);
+                } else
+                {
+                    i++;
+                }
+            }
+
+            for (i = 0; i < arr.length; i++)
+            {
+                if (arr[i] != i + 1) {
+                    duplicates.add(arr[i]);
+                }
+            }
+
+            return duplicates;
+        }
+
+        static void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+
